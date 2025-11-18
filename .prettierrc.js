0ebb28292550f6783cb911f843ec54f24d5348a7
futoshi-takahashi <f_takahashi@emotional.jp>
2025-11-19 05:20:45 +0900

    フォーマッタ設定

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  printWidth: 80,
+  trailingComma: 'none',
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  bracketSameLine: false,
+  arrowParens: 'avoid',
+  importOrderSeparation: true
+}
