3e41187571ab2036d58009efeabd7036a42790c4
futoshi-takahashi <f_takahashi@emotional.jp>
2025-12-13 12:26:05 +0900

    文面修正

--- frontend/src/ProfileLayout/components/ProfileFooter/Presentational.tsx
+++ frontend/src/ProfileLayout/components/ProfileFooter/Presentational.tsx
@@ -1,14 +1,5 @@
 export const Presentational = () => (
   <footer className="mt-16 pt-4 border-t border-slate-800 text-slate-500 text-sm text-right">
-    デザイン参考:
-    <a
-      href="https://brittanychiang.com/"
-      target="_blank"
-      rel="noopener noreferrer"
-      className="text-cyan-400 hover:text-cyan-300 underline ml-1"
-    >
-      Brittany Chiangさんのスキルシート
-    </a>
     <br />
     このページのソースコード:
     <a
